805475004f30a2f4a3a7d193f05700a98a2f557a
krzysztofdabkowski1 <33841285+krzysztofdabkowski1@users.noreply.github.com>
2017-11-30 23:36:17 +0100

    plansza

--- Gra_wyskokowa/gra_wyskokowa.c
+++ Gra_wyskokowa/gra_wyskokowa.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
 #include <allegro5/allegro.h>
 #include <allegro5/allegro_font.h>
 #include <allegro5/allegro_ttf.h>
 #include <allegro5/allegro_color.h>
+#include <allegro5/allegro_primitives.h>
 const float FPS = 60;
 const int SCREEN_W = 1000;
 const int SCREEN_H = 700;
@@ -11,9 +14,9 @@ int main()
 	al_init();
 	al_init_font_addon(); 
 	al_init_ttf_addon();
-	
+	al_init_primitives_addon();
 	ALLEGRO_DISPLAY * display = al_create_display(SCREEN_W, SCREEN_H);
-	ALLEGRO_FONT* font = al_load_ttf_font("arial.ttf", 32, 0),*font2=al_load_ttf_font("arial.ttf",66,0);
+	ALLEGRO_FONT* font = al_load_ttf_font("arial.ttf", 32, 0),*font2=al_load_ttf_font("arial.ttf",66,0), *font3= al_load_ttf_font("arial.ttf", 20, 0);
 	ALLEGRO_EVENT_QUEUE *event_queue = NULL;
 	ALLEGRO_TIMER *timer = NULL;
 	ALLEGRO_BITMAP *kursor = al_create_bitmap(0,0);
@@ -21,6 +24,7 @@ int main()
 	float kursor_y = 350;
 	bool redraw = true;
 	bool on = true;
+	int a;
 	timer = al_create_timer(1.0 / FPS);
 	al_install_mouse();
     al_clear_to_color(al_map_rgb(255, 0, 255));
@@ -39,7 +43,7 @@ int main()
 	al_flip_display();
 
 	al_start_timer(timer);
-	while (1)
+	while (on)
 	{
 		ALLEGRO_EVENT ev;
 		al_wait_for_event(event_queue, &ev);
@@ -56,10 +60,11 @@ int main()
 			kursor_x= ev.mouse.x;
 			kursor_y=ev.mouse.y;
 		}
-		////if (ev.type == ALLEGRO_EVENT_MOUSE_BUTTON_UP)
-		//{
-		////	break;		
-		//}
+		if (ev.type == ALLEGRO_EVENT_MOUSE_BUTTON_DOWN&&(kursor_x > 470 && kursor_x< 530 && kursor_y>350 && kursor_y < 400))
+		{
+			on = false;
+			a=graj(display,font3);
+		}
 
 		if (redraw && al_is_event_queue_empty(event_queue)) {
 			redraw = false;
@@ -91,15 +96,187 @@ int main()
 
 			if (kursor_x > 450 && kursor_x < 550 && kursor_y>550 && kursor_y < 600)
 			{
-				al_draw_text(font, al_map_rgb(255, 0, 0), 500, 550, ALLEGRO_ALIGN_CENTRE, "WyjdŸ");
+				al_draw_text(font, al_map_rgb(255, 0, 0), 500, 550, ALLEGRO_ALIGN_CENTRE, "Wyjdz");
 			}
-			else al_draw_text(font, al_map_rgb(0, 0, 0), 500, 550, ALLEGRO_ALIGN_CENTRE,"WyjdŸ");
-			al_flip_display(display);
+			else al_draw_text(font, al_map_rgb(0, 0, 0), 500, 550, ALLEGRO_ALIGN_CENTRE,"Wyjdz");
+			al_flip_display();
 		}
 	}
 	system("pause");
 	
 	al_destroy_event_queue(event_queue);
     al_destroy_display(display);
+	return 0;
+}
+int graj(ALLEGRO_DISPLAY *display, ALLEGRO_FONT *font3)
+{
+	int x=1000, y=150;
+	int i = 0,t=0;
+	int start=0;
+	ALLEGRO_BITMAP *player = al_create_bitmap(10, 10);
+	ALLEGRO_TIMER *timer = al_create_timer(1.0 / FPS);
+	ALLEGRO_EVENT_QUEUE *event_queue = NULL;
+	bool redraw = true;
+	int etap = 0, tak=0;
+	float array[8] = { 1000,230,900,300,800,370,670,385 };
+	float array2[8] = {1000,350,450,375,370,620,0,650};
+	float dest2[240];
+	float dest3[8]; 
+	float dest4[8];
+	float dest5[8];
+	float line[240];
+	float line2[1000];
+	
+	al_calculate_spline(line, 8, array, 4, 60);
+	al_calculate_spline(dest2, 8, array2, 0.5, 60);
+	al_calculate_spline(line2, 8, array2, 0.5, 100);
+
+	dest2[0] = 0;
+	dest2[1] = 700;
+	dest2[2] = 1000;
+	dest2[3] = 700;
+	dest2[4] = 1000;
+	dest2[5] = 350;
+
+	dest3[0] = 990;
+	dest3[1] = 360;
+	dest3[2] = 990;
+	dest3[3] = 238;
+	dest3[4] = 970;
+	dest3[5] = 250;
+	dest3[6] = 970;
+	dest3[7] = 370;
+
+    dest4[0] = 900;
+	dest4[1] = 360;
+	dest4[2] = 900;
+	dest4[3] = 298;
+	dest4[4] = 880;
+	dest4[5] = 310;
+	dest4[6] = 880;
+	dest4[7] = 360;
+	
+	dest5[0] = 720;
+	dest5[1] = 450;
+	dest5[2] = 720;
+	dest5[3] = 375;
+	dest5[4] = 700;
+	dest5[5] = 380;
+	dest5[6] = 700;
+	dest5[7] = 450;
+
+	al_clear_to_color(al_map_rgb(0, 127, 255));
+	al_set_target_bitmap(player);
+	al_clear_to_color(al_map_rgb(0, 0, 0));
+	al_set_target_bitmap(al_get_backbuffer(display));
+	event_queue = al_create_event_queue();
+	al_register_event_source(event_queue, al_get_display_event_source(display));
+	al_register_event_source(event_queue, al_get_timer_event_source(timer));
+	al_start_timer(timer);
+	while (1)
+	{
+		ALLEGRO_EVENT ev;
+		al_wait_for_event(event_queue, &ev);
+
+		if (ev.type == ALLEGRO_EVENT_TIMER) {
+			if (etap==0) 
+			{
+				redraw = true;
+				x = line[4 * i];
+				y = line[4 * i + 1] - 15;
+				if (x <= 670)
+				{
+					etap = 1;
+				}
+				i++;
+				
+				
+
+		    }
+			
+		    if(etap==1){
+				redraw = true;
+				x = x - 8 + t / 16;
+				y = y + t / 7;
+				t++;
+				for (int i = 0; i < 400; i=i+2)
+				{
+					if ((line2[i] > x)&&(line2[i] < x + 10))
+					{
+						if ((line2[i + 1] > y)&&(line2[i + 1] < y + 10))
+						{
+						//	printf("detection");
+							//printf(" %i , %i , %f , %f,%i", x, y, line2[i], line2[i + 1],i);
+							tak = i;
+							etap = 2;
+							t = 0;
+		
+							
+						}
+					}
+				}
+				
+			}
+			if (etap == 2)
+			{
+				redraw = true;
+					if (x>0)
+					{
+						x = line2[tak+2*start];
+						y = line2[tak + 1+2*start]-10;
+						if (floor(0.07*t) <= 2)
+						{
+							start = start + 2-floor(0.07*t);
+							
+						}
+						printf("%i, %i\n",x, y);
+						
+                     t++;
+					}
+					
+
+				
+				
+			}
+		}
+		
+		//printf("%f\n", line2[i]);
+
+		
+		    if (ev.type == ALLEGRO_EVENT_DISPLAY_CLOSE) {
+				break;
+		    }
+			if (redraw && al_is_event_queue_empty(event_queue)) {
+				redraw = false;
+				al_clear_to_color(al_map_rgb(0, 127, 255));
+				if (x > 0 && y < 700) {
+					al_draw_bitmap(player, x, y, 0);
+				}
+				
+				al_draw_spline(array2, al_map_rgb(255, 255, 255), 2);
+				al_draw_spline(array, al_map_rgb(255, 255, 255), 4);
+				al_draw_filled_polygon(dest2, 120, al_map_rgb(255, 255, 255));
+
+				al_draw_filled_polygon(dest3, 4, al_map_rgb(255, 255, 255));
+				al_draw_filled_polygon(dest4, 4, al_map_rgb(255, 255, 255));
+				al_draw_filled_polygon(dest5, 4, al_map_rgb(255, 255, 255));
+				al_draw_filled_rectangle(10, 10, 150, 40, al_map_rgb(0, 100, 0));
+				al_draw_filled_rectangle(900, 10, 990, 40, al_map_rgb(110, 100, 110));
+				al_draw_filled_rectangle(211, 601, 215, 605, al_map_rgb(255, 0, 0));
+				al_draw_filled_rectangle(277, 575, 281, 579, al_map_rgb(0, 255, 0));
+				al_draw_text(font3, al_map_rgb(255, 0, 0), 15, 20, ALLEGRO_ALIGN_LEFT, "tu bedzie kiedys predkosc wiatru");
+				al_draw_text(font3, al_map_rgb(255, 0, 0), 905, 20, ALLEGRO_ALIGN_CENTRE, "5 sekund na start");
+				al_flip_display();
+			}
+
+			
+
+		}
+
+
+	
+	
+	
+	
 	return 0;
 }
